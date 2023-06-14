d0fd4834ff912f982d89350f8bd573a9bec34faf
AlefNatan <104284234+AlefNatan@users.noreply.github.com>
2023-06-13 21:32:17 -0300

    Update Orcamento.js
    Adicionado novas funcionalidades as celulas

--- Orcamento.js
+++ Orcamento.js
@@ -8,42 +8,48 @@ window.addEventListener("DOMContentLoaded", function() {
     dataAtualElement.innerText = formatoData;
   });
 
-window.addEventListener("DOMContentLoaded", function() {
-  const divs = document.querySelectorAll('.container');
+  window.addEventListener("DOMContentLoaded", function() {
+    const divs = document.querySelectorAll('.container');
+  
+    for (let i = 1; i < divs.length; i++) {
+      divs[i].classList.add('hidden');
+    }
+  
+    const divsArray = Array.from(divs); // Converter a NodeList em um array para usar o método indexOf()
+  
+    divsArray.forEach(function(div, index) {
+      const adicionar = div.querySelector('img[alt="Adicionar"]');
+      const lixeira = div.querySelector('img[alt="lixeira"]');
+      const celulaSeguinte = divsArray[index + 1];
+  
+      adicionar.addEventListener('click', function() {
+        adicionar.classList.add('hidden');
+        lixeira.classList.add('hidden');
+        if (celulaSeguinte) {
+          celulaSeguinte.classList.remove('hidden');
+        }
+      });
+  
+      lixeira.addEventListener('click', function() {
+        const celula = this.parentNode;
+        celula.classList.add('hidden');
+        const celulaAnterior = divsArray[index - 1];
+        if (celulaAnterior) {
+          const adicionarCelulaAnterior = celulaAnterior.querySelector('img[alt="Adicionar"]');
+          const lixeiraCelulaAnterior = celulaAnterior.querySelector('img[alt="lixeira"]')
+          adicionarCelulaAnterior.classList.remove('hidden');
+          lixeiraCelulaAnterior.classList.remove('hidden');
+        }
+       // Limpar campos da div
+        const camposDiv = celula.querySelectorAll('input[type="text"]');
+        camposDiv.forEach(function(campo) {
+        campo.value = ' ';
+        campo.dispatchEvent(new Event('input', { bubbles: true }));
+        });
 
-  for (let i = 1; i < divs.length; i++) {
-    divs[i].classList.add('hidden');
-  }
-
-  const divsArray = Array.from(divs); // Converter a NodeList em um array para usar o método indexOf()
-
-  divsArray.forEach(function(div, index) {
-    const adicionar = div.querySelector('img[alt="Adicionar"]');
-    const lixeira = div.querySelector('img[alt="lixeira"]');
-    const celulaSeguinte = divsArray[index + 1];
-
-    adicionar.addEventListener('click', function() {
-      adicionar.classList.add('hidden');
-      if (celulaSeguinte) {
-        celulaSeguinte.classList.remove('hidden');
-      }
+      });
     });
-
-    lixeira.addEventListener('click', function() {
-      const celula = this.parentNode;
-      celula.classList.add('hidden');
-      const celulaAnterior = divsArray[index - 1];
-      if (celulaAnterior) {
-        const adicionarCelulaAnterior = celulaAnterior.querySelector('img[alt="Adicionar"]');
-        adicionarCelulaAnterior.classList.remove('hidden');
-      }
-      if (index === divsArray.length - 1) {
-        const ultimaCelula = divsArray[divsArray.length];
-        ultimaCelula.classList.remove('hidden');
-      }
-    });
-  });
-});
+  });  
 
 window.addEventListener("DOMContentLoaded", function() {
     // Selecionando todas as divs com a classe "container"
@@ -71,25 +77,36 @@ window.addEventListener("DOMContentLoaded", function() {
   });
   
   window.addEventListener("DOMContentLoaded", function() {
-    // Obtendo todas as divs com classe "container"
     var divs = document.querySelectorAll(".container");
   
-    // Função para atualizar a multiplicação e calcular o subtotal
-    function atualizarMultiplicacao() {
-      var subtotal = 0; // Variável para armazenar a soma dos resultados
+    function formatarReais(valor) {
+      var formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2
+      });
   
-      // Iterando sobre todas as divs
+      return formatter.format(valor);
+    }
+  
+    function formatarPorcentagem() {
+      var porcInput = document.getElementById("porc-input");
+      var valor = porcInput.value.replace("%", "");
+      porcInput.value = valor + "%";
+    }
+  
+    function atualizarMultiplicacao() {
+      var subtotal = 0;
+      var desconto = 0;
+  
       divs.forEach(function(div) {
-        // Obtendo os elementos dentro de cada div
         var input1 = div.querySelector(".input1");
         var input3 = div.querySelector(".input3");
         var resultado = div.querySelector(".p");
   
-        // Convertendo os valores para números
-        var valor1 = parseFloat(input1.value);
-        var valor3 = parseFloat(input3.value);
+        var valor1 = parseFloat(input1.value.replace(",", "."));
+        var valor3 = parseFloat(input3.value.replace(",", "."));
   
-        // Verificando se os valores são válidos
         if (isNaN(valor1)) {
           valor1 = 0;
         }
@@ -97,33 +114,60 @@ window.addEventListener("DOMContentLoaded", function() {
           valor3 = 0;
         }
   
-        // Realizando a multiplicação
         var multiplicacao = valor1 * valor3;
   
-        // Atualizando o conteúdo do elemento <p>
-        resultado.innerHTML = multiplicacao.toFixed(2);
+        resultado.innerHTML = formatarReais(multiplicacao);
   
-        // Somando o resultado ao subtotal
         subtotal += multiplicacao;
       });
   
-      // Exibindo o subtotal na div desejada
+      var porcInput = document.getElementById("porc-input");
+      var porcentagem = parseFloat(porcInput.value);
+  
+      if (isNaN(porcentagem) || porcentagem < 0) {
+        porcentagem = 0;
+      } else if (porcentagem > 100) {
+        porcentagem = 100;
+        porcInput.value = "100%";
+      }
+  
+      desconto = (subtotal * porcentagem) / 100;
+  
+      var descontoElement = document.getElementById("val");
+      descontoElement.innerHTML = formatarReais(desconto);
+  
       var subtotalElement = document.querySelector(".valor");
-      subtotalElement.innerHTML = subtotal.toFixed(2);
+      subtotalElement.innerHTML = formatarReais(subtotal);
+  
+      var valTotElement = document.querySelector(".ValTot");
+      var valorTotal = subtotal - desconto;
+      valTotElement.innerHTML = formatarReais(valorTotal);
     }
   
-    // Adicionando o evento de escuta nos inputs de todas as divs
     divs.forEach(function(div) {
       var input1 = div.querySelector(".input1");
       var input3 = div.querySelector(".input3");
       input1.addEventListener("input", atualizarMultiplicacao);
-      input3.addEventListener("input", atualizarMultiplicacao);
+  
+      input3.addEventListener("input", function() {
+        // Verifica se o valor inserido possui mais de duas casas decimais
+        var inputValue = input3.value;
+        if (/\.\d{3,}$/.test(inputValue)) {
+          // Remove qualquer dígito além das duas casas decimais
+          input3.value = parseFloat(inputValue).toFixed(2);
+        }
+        atualizarMultiplicacao();
+      });
     });
   
-    // Chamando a função inicialmente para exibir os resultados
+    var porcInput = document.getElementById("porc-input");
+    porcInput.addEventListener("input", function() {
+      formatarPorcentagem();
+      atualizarMultiplicacao();
+    });
+  
     atualizarMultiplicacao();
   });
-  
   
   
 
